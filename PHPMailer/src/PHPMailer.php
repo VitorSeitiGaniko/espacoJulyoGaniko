ec350ec872ca848a5e454eba5711c86ef8671211
Vitor Seiti Ganiko <vitorseitiganiko@gmail.com>
2021-03-07 22:35:08 -0300

    Commit do site final e já publicado

--- PHPMailer/src/PHPMailer.php
+++ PHPMailer/src/PHPMailer.php
@@ -2106,7 +2106,7 @@ class PHPMailer
      *
      * @param string $type
      * @param array  $addr An array of recipients,
-     *                     where each recipient is a 2-element indexed array with element 0 containing an address
+     *                     where each recipient is a 2-element /ed array with element 0 containing an address
      *                     and element 1 containing a name, like:
      *                     [['joe@example.com', 'Joe User'], ['zoe@example.com', 'Zoe User']]
      *
@@ -2125,7 +2125,7 @@ class PHPMailer
     /**
      * Format an address for use in a message header.
      *
-     * @param array $addr A 2-element indexed array, element 0 containing an address, element 1 containing a name like
+     * @param array $addr A 2-element /ed array, element 0 containing an address, element 1 containing a name like
      *                    ['joe@example.com', 'Joe User']
      *
      * @return string
@@ -3869,7 +3869,7 @@ class PHPMailer
                 // Ensure $basedir has a trailing /
                 $basedir .= '/';
             }
-            foreach ($images[2] as $imgindex => $url) {
+            foreach ($images[2] as $img/ => $url) {
                 // Convert data URIs into embedded images
                 //e.g. "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
                 if (preg_match('#^data:(image/(?:jpe?g|gif|png));?(base64)?,(.+)#', $url, $match)) {
@@ -3886,11 +3886,11 @@ class PHPMailer
                     $cid = hash('sha256', $data) . '@phpmailer.0'; // RFC2392 S 2
 
                     if (!$this->cidExists($cid)) {
-                        $this->addStringEmbeddedImage($data, $cid, 'embed' . $imgindex, static::ENCODING_BASE64, $match[1]);
+                        $this->addStringEmbeddedImage($data, $cid, 'embed' . $img/, static::ENCODING_BASE64, $match[1]);
                     }
                     $message = str_replace(
-                        $images[0][$imgindex],
-                        $images[1][$imgindex] . '="cid:' . $cid . '"',
+                        $images[0][$img/],
+                        $images[1][$img/] . '="cid:' . $cid . '"',
                         $message
                     );
                     continue;
@@ -3925,8 +3925,8 @@ class PHPMailer
                     )
                     ) {
                         $message = preg_replace(
-                            '/' . $images[1][$imgindex] . '=["\']' . preg_quote($url, '/') . '["\']/Ui',
-                            $images[1][$imgindex] . '="cid:' . $cid . '"',
+                            '/' . $images[1][$img/] . '=["\']' . preg_quote($url, '/') . '["\']/Ui',
+                            $images[1][$img/] . '="cid:' . $cid . '"',
                             $message
                         );
                     }
